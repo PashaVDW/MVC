f66b229fb2f45377312349c8945b4d5f236a9960
Cfuzion <pasha.pasha12e@gmail.com>
2023-03-09 20:30:53 +0100

    added routing but Fatal error: Uncaught Exception: No route found for URL : / in C:\Users\Pasha\dev\MVC\public\index.php:21 Stack trace: #0 {main} thrown in C:\Users\Pasha\dev\MVC\public\index.php on line 21

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "hello";
+
+require_once '../routes/router.php';
+
+$router = new Router();
+
+// Add the routes
+$router->add('', ['controller' => 'home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'posts', 'action' => 'new']);
+
+// Match the requested route
+$url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
+
+if($router->match($url)) {
+    $params = $router->getParams();
+    $controller = $params['controller'];
+    $action = $params['action'];
+    echo "Controller: $controller, Action: $action";
+} else {
+    throw new Exception("No route found for URL : " . $url);
+}
