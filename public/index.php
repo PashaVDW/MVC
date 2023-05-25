1ae6c2a305dc499f61e25c2348488fcdf45a0982
Cfuzion <pasha.pasha12e@gmail.com>
2023-05-25 16:50:22 +0200

    working on MVC

--- public/index.php
+++ public/index.php
@@ -1,22 +1,2 @@
 <?php
 
-require_once '../routes/router.php';
-
-$router = new Router();
-
-// Add the routes
-$router->add('', ['controller' => 'home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'posts', 'action' => 'new']);
-
-// Match the requested route
-$url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
-
-if($router->match($url)) {
-    $params = $router->getParams();
-    $controller = $params['controller'];
-    $action = $params['action'];
-    echo "Controller: $controller, Action: $action";
-} else {
-    throw new Exception("No route found for URL : " . $url);
-}
