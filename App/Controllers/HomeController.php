f8b2a7c71f27e4eab09651e16c1e10f02d142a39
Cfuzion <pasha.pasha12e@gmail.com>
2023-06-03 16:02:52 +0200

    middleware

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -12,6 +12,7 @@ class HomeController
     }
 
     public function index() {
+
         Router::renderTemplate('/layouts/app.view.php');
     }
 }
---
+++ Core/middleware/Middleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware
+{
+
+    /**
+     * @param $key
+     *
+     * @return void
+     */
+    public static function resolve($key)
+    {
+        if (!$key)
+        {
+            return;
+        }
+    }
+
+}
